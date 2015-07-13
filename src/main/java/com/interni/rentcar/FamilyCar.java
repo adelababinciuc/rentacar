a156233e42bf277f73f8a8380f0daeba555f4ff2
AlexandruDumitriu <alex.dumitriu@levi9.com>
2015-07-13 16:07:50 +0300

    familyCar v1

--- src/main/java/com/interni/rentcar/FamilyCar.java
+++ src/main/java/com/interni/rentcar/FamilyCar.java
@@ -1,5 +1,22 @@
 package com.interni.rentcar;
 
 public class FamilyCar extends Car{
+	
+	private boolean trailer;
+	
+	public FamilyCar(){
+		
+	}
 
+	public boolean isTrailer() {
+		return trailer;
+	}
+
+	public void setTrailer(boolean trailer) {
+		this.trailer = trailer;
+	}
+	
+	
+	
+	
 }
