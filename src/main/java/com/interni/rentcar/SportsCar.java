cb28fb5dec41e2f5df61ffd9473c08a601f6d6eb
SStefanVlad <s.sinescu@levi9.com>
2015-07-13 16:04:02 +0300

    SportsCar commit vers 1.0

--- src/main/java/com/interni/rentcar/SportsCar.java
+++ src/main/java/com/interni/rentcar/SportsCar.java
@@ -1,5 +1,18 @@
 package com.interni.rentcar;
 
-public class SportsCar extends Car{
+public class SportsCar extends Car {
+	private boolean sportsCarTurboBoost;
+
+	public SportsCar() {
+		super();
+	}
+
+	public boolean isSportsCarTurboBoost() {
+		return sportsCarTurboBoost;
+	}
+
+	public void setSportsCarTurboBoost(boolean sportsCarTurboBoost) {
+		this.sportsCarTurboBoost = sportsCarTurboBoost;
+	}
 
 }
